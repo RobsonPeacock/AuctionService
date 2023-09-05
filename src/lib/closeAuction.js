d9632a0eb32cb2a9f4155cde4cf4972f23eab216
Robson Peacock <robson_peacock20@outlook.com>
2023-09-05 11:30:54 +0100

    Add condition for when an auction receives no bids

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -21,6 +21,19 @@ export async function closeAuction(auction) {
   const { title, seller, highestBid } = auction;
   const { amount, bidder } = highestBid;
 
+  if (amount === 0) {
+    await sqs.sendMessage({
+      QueueUrl: process.env.MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'Your listing expired',
+        recipient: seller,
+        body: `Your item "${title}" did not recieve any bids`,
+      }),
+    }).promise();
+
+    return;
+  }
+
   const notifySeller = sqs.sendMessage({
     QueueUrl: process.env.MAIL_QUEUE_URL,
     MessageBody: JSON.stringify({
